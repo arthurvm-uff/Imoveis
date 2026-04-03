ac21527815180ba83307c7736bbc3122cb9b0239
arthurvm-uff <arthurvm@id.uff.br>
2026-04-03 14:03:53 -0300

    Verificação da análise descritiva e criação de Gráficos(Histograma e Gráfico de dispersão)

--- analise-simples.R
+++ analise-simples.R
@@ -7,3 +7,38 @@ usethis::use_git_config(
   user.name = "arthurvm-uff",
   user.email = "arthurvm@id.uff.br"
 )
+
+#Análise descritiva das variáveis:
+summary(preco)
+sd(preco)
+
+summary(metragem)
+sd(metragem)
+
+summary(idade)
+sd(idade)
+
+summary(itens_embutidos)
+sd(itens_embutidos)
+
+summary(imposto_anual)
+sd(imposto_anual)
+
+#Histograma dos preços dos imóveis:
+ggplot(dados, aes(x=preco)) +
+  geom_histogram(fill="steelblue", color="white", bins = 20) +
+  labs(
+    title="Distribuição dos preços dos Imóveis",
+    x="Preço(R$)",
+    y="Frequência"
+  )
+
+#Gráfico de dispersão (Imposto x Metragem):
+ggplot(dados, aes(x = metragem, y = imposto_anual)) +
+  geom_point(alpha = 0.5, color = "darkorange") +
+  geom_smooth(method = "lm", color = "black") +
+  labs(
+    title = "Relação entre Metragem e Imposto",
+    x = "Metragem (m²)",
+    y = "Imposto (R$)"
+  )
