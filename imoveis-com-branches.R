7a91ec6808a16f211ff02400855363de3d2a57f0
arthurvm-uff <arthurvm@id.uff.br>
2026-04-10 17:09:06 -0300

    Script Esqueleto

---
+++ imoveis-com-branches.R
@@ -0,0 +1,12 @@
+#Script Esqueleto
+#Etapa 1: Leitura do banco de dados.
+dados=read.csv("imoveis.csv", header=TRUE, sep=";")
+
+str(dados)
+head(dados)
+
+#Etapa 2: Criação de gráficos.
+#Preencher na branch
+
+#Etapa 3: Análise de medidas.
+#Preencher na branch
